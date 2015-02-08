295a03c2dc1b157501af3c36f066c8ea7714d4e9
marcanpilami <marsu_pilami@msn.com>
2015-02-08 14:45:08 +0100

    envt screen light de-clutter

--- ref/views/display.py
+++ ref/views/display.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from django.shortcuts import render
-from ref.models.instances import Environment
+from ref.models.instances import Environment, ComponentInstance
 from ref.models.parameters import getParam
 
 
@@ -14,9 +14,13 @@ def templates(request):
 def envt(request, envt_id):
     envt = Environment.objects.\
                     select_related('typology').\
-                    prefetch_related('component_instances__field_set__field').\
-                    prefetch_related('component_instances__rel_target_set').\
-                    prefetch_related('component_instances__description__computed_field_set').\
-                    prefetch_related('component_instances__instanciates__implements__application').\
-                    get(pk=envt_id)
-    return render(request, 'ref/envt.html', {'envt': envt, })
+                    get(pk=envt_id)    
+    deleted = ComponentInstance.objects.filter(environments__id=envt_id, deleted=True).select_related('description').order_by('description__name', 'id')
+    cis = ComponentInstance.objects.filter(environments__id=envt_id, deleted=False).\
+                    select_related('description').\
+                    prefetch_related('field_set__field').\
+                    prefetch_related('rel_target_set').\
+                    prefetch_related('description__computed_field_set').\
+                    prefetch_related('instanciates__implements__application')   
+    
+    return render(request, 'ref/envt.html', {'envt': envt, 'deleted': deleted, 'cis' : cis})
