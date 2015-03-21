a5d01b1620bd55c28d6d32477ca5586cb4348cb6
marcanpilami <marsu_pilami@msn.com>
2015-03-21 12:42:29 +0100

    Refactored CI edit step 2

--- ref/views/envt_new.py
+++ ref/views/envt_new.py
@@ -14,11 +14,26 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.db.models.query import Prefetch
 
+''' 
+    This file contains views and form for the standard component instance creation and update. (unitary forms)
+
+    Creation is divided in two forms so as to be able to apply naming conventions during creation:
+    * first step asks for the environment and linked items, for these could be used in convention
+    * naming convention is applied and component instance is created with only this data 
+      (may result in an incorrect CI for compulsory fields may still be null after applying conventions)
+    * second step allows to edit the same data, plus all fields
+'''
 
 def new_items(request):
     """Hub for creating all sorts of items"""
     return render(request, 'ref/ref_new_items.html', {'impls': ImplementationDescription.objects.all()})
 
+
+#####################################################################
+## STEP 1: envt + relationships
+#####################################################################
+
+## Views
 @permission_required('ref.scm_addcomponentinstance')
 @atomic
 def new_ci_step1(request, description_id):
@@ -37,68 +52,7 @@ def new_ci_step1(request, description_id):
 
     return render_to_response("ref/instance_edit_step1.html", {'form': form, 'descr' : descr})
 
-@permission_required('ref.scm_addcomponentinstance')
-@atomic
-def new_ci_step2(request, instance_id):
-    instance = ComponentInstance.objects.select_related('description', 'implements')\
-            .prefetch_related(
-                  Prefetch('field_set', ComponentInstanceField.objects.order_by('field__widget_row', 'field__name').select_related('field')),
-                  Prefetch('rel_target_set', ComponentInstanceRelation.objects.order_by('field__name').select_related('field')))\
-            .prefetch_related('environments', 'description__field_set', 'description__target_set').get(pk=instance_id)
-    descr = instance.description
-    cls = form_for_model(descr)
-
-    if request.POST:
-        form = cls(request.POST)
-        if form.is_valid():
-            instance.deleted = form.cleaned_data['_deleted']
-            instance.environments = form.cleaned_data['_envts']
-            instance.instanciates = form.cleaned_data['_instanciates']
-            instance.include_in_envt_backup = form.cleaned_data['_backup']
-            
-            for field in descr.field_set.all():
-                if not field.name in form.changed_data:
-                    continue
-                new_data = form.cleaned_data[field.name]
-                ComponentInstanceField.objects.update_or_create(defaults={'value': new_data} , field=field, instance=instance)
-
-                for field in descr.target_set.all():
-                    if not field.name in form.changed_data:
-                        continue
-                    new_data = form.cleaned_data[field.name]
-                    ComponentInstanceRelation.objects.update_or_create(defaults={'target': new_data}, source=instance, field=field)
-                
-            instance.save()
-    else:
-        di = {'_id': instance.pk, '__descr_id': instance.description_id, '_deleted': instance.deleted, '_backup': instance.include_in_envt_backup,
-              '_instanciates' : instance.instanciates_id, '_envts' : [e.id for e in instance.environments.all()]}
-
-        for field_instance in instance.field_set.all():
-            di[field_instance.field.name] = field_instance.value
-
-        for field_instance in instance.rel_target_set.all():
-            di[field_instance.field.name] = field_instance.target_id
-
-        form = cls(di)  # unbound form
-
-    return render_to_response("ref/instance_edit_step2.html", {'form': form, 'descr' : descr, 'instance': instance})
-
-
-
-class NewEnvtForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    description = forms.CharField(max_length=100)
-
-class CicChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return '%s - [%s] - offre [%s]' % (obj.implements.application, obj.implements.name, obj.name)
-
-class CiChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return ' %s - %s (%s)' % (obj.first_environment(), obj.name, obj.instanciates.name if obj.instanciates else 'none')
-
-
-## Forms for item creation (step 1)
+## Forms
 class NewCiStep1Form(forms.Form):
     _env = forms.ModelChoiceField(queryset=Environment.objects.all(), label='Environnement', required=False)
 
@@ -108,6 +62,13 @@ class NewCiStep1Form(forms.Form):
     helper.field_class = 'col-lg-9'
     helper.add_input(Submit('submit', 'Submit'))
 
+class CicChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return '%s - [%s] - offre [%s]' % (obj.implements.application, obj.implements.name, obj.name)
+
+class CiChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return ' %s - %s (%s)' % (obj.first_environment(), obj.name, obj.instanciates.name if obj.instanciates else 'none')
 
 def form_for_model_relations(descr):
     attrs = {}
@@ -119,24 +80,80 @@ def form_for_model_relations(descr):
 
     # Relations
     for field in descr.target_set.filter(max_cardinality__lte=1).prefetch_related('target__instance_set'):
-        f = CiChoiceField(queryset=field.target.instance_set.order_by('environments__name').prefetch_related('environments'), label=field.label, required=field.min_cardinality == 1)
+        f = CiChoiceField(queryset=field.target.instance_set.order_by('environments__name').select_related('instanciates').prefetch_related('environments'), label=field.label, required=field.min_cardinality == 1)
         attrs[field.name] = f
 
     cls = type(str("__" + descr.name.lower() + "_form"), (NewCiStep1Form,), attrs)
     return cls
 
-## Forms for single item edition (also 2nd step of creation)
-class ModelChoiceFieldCIC(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return "%s (%s)" % (obj.name, obj.description)
 
-class MiniModelForm(forms.Form):
-    _id = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    _deleted = forms.BooleanField(required=False, label='effacé')
-    _backup = forms.BooleanField(required = False, label='inclus dans backup')
-    _instanciates = ModelChoiceFieldCIC(queryset=ComponentImplementationClass.objects, required=False, label='offre implémentée')
-    _envts = forms.ModelMultipleChoiceField(queryset=Environment.objects.order_by('name').filter(template_only=False, active=True), required=False, label='environnements')    
+#####################################################################
+## STEP 2: all fields
+#####################################################################
 
+# Views
+@permission_required('ref.scm_addcomponentinstance')
+@atomic
+def new_ci_step2(request, instance_id):  # always edit an existing CI - to create a CI use step 1.
+    instance = ComponentInstance.objects.select_related('description', 'implements')\
+            .prefetch_related(
+                  Prefetch('field_set', ComponentInstanceField.objects.order_by('field__widget_row', 'field__name').select_related('field')),
+                  Prefetch('rel_target_set', ComponentInstanceRelation.objects.order_by('field__name').select_related('field')))\
+            .prefetch_related('environments', 'description__field_set', 'description__target_set').get(pk=instance_id)
+    cls = form_for_model(instance.description)
+
+    if request.POST:
+        form = cls(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+    else:
+        form = cls(instance=instance)
+
+    return render_to_response("ref/instance_edit_step2.html", {'form': form, 'descr' : instance.description, 'instance': instance})
+
+
+# Forms
+class FullCIEditFormBase(forms.ModelForm):
+    class Meta:
+        model = ComponentInstance
+        fields = ['deleted', 'include_in_envt_backup', 'instanciates', 'environments', ]
+        
+    def __init__(self, *args, **kwargs):
+        super(FullCIEditFormBase, self).__init__(*args, **kwargs)
+        
+        ## Restrict some choices
+        self.fields["environments"].queryset = Environment.objects.order_by('name').filter(template_only=False, active=True)
+        self.fields["instanciates"].queryset = ComponentImplementationClass.objects.order_by('implements__application__name', 'name').filter(active=True, implements__active=True)
+
+        ## Stupid: self.instance can be overridden by a field named instance... So we store it inside another field (with a forbidden name)
+        self.mage_instance = self.instance
+
+        ## Init dynamic fields values
+        for field_instance in self.instance.rel_target_set.all():
+            self.fields[field_instance.field.name].initial = field_instance.target_id
+
+        for field_instance in self.instance.field_set.all():
+            self.fields[field_instance.field.name].initial = field_instance.value
+    
+    def save(self, commit=True):
+        if len(self.changed_data) == 0:
+            return
+        
+        for field in self.mage_instance.description.field_set.all():
+            if not field.name in self.changed_data:
+                    continue
+            new_data = self.cleaned_data[field.name]
+            ComponentInstanceField.objects.update_or_create(defaults={'value': new_data} , field=field, instance=self.mage_instance)
+
+        for field in self.mage_instance.description.target_set.all():
+            if not field.name in self.changed_data:
+                continue
+            new_data = self.cleaned_data[field.name]
+            ComponentInstanceRelation.objects.update_or_create(defaults={'target': new_data}, source=self.mage_instance, field=field)
+
+        super(FullCIEditFormBase, self).save(commit=commit)
+
+            
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
     helper.label_class = 'col-lg-2'
@@ -158,4 +175,4 @@ def form_for_model(descr):
         f = forms.ModelChoiceField(queryset=field.target.instance_set, label=field.label, required=field.min_cardinality == 1)
         attrs[field.name] = f
 
-    return type(str("__" + descr.name.lower() + "_form"), (MiniModelForm,), attrs)
+    return type(str("__" + descr.name.lower() + "_form"), (FullCIEditFormBase,), attrs)
