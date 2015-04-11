2cab1efab8fdb821b5027ab68b4eba7230ced29a
marcanpilami <marsu_pilami@msn.com>
2015-04-11 15:19:07 +0200

    Envt page refactored for performance

--- ref/views/display.py
+++ ref/views/display.py
@@ -1,8 +1,13 @@
 # coding: utf-8
 
 from django.shortcuts import render
-from ref.models.instances import Environment, ComponentInstance
+from django.db.models.query import Prefetch
+
+from ref.models.instances import Environment, ComponentInstance, \
+    ComponentInstanceField
 from ref.models.parameters import getParam
+from ref.models.description import ImplementationFieldDescription, \
+    ImplementationComputedFieldDescription
 
 
 def envts(request):
@@ -14,16 +19,26 @@ def templates(request):
 def envt(request, envt_id):
     envt = Environment.objects.\
                     select_related('typology').\
-                    get(pk=envt_id)    
-    deleted = ComponentInstance.objects.filter(environments__id=envt_id, deleted=True).select_related('description').order_by('description__name', 'id')
+                    get(pk=envt_id)   
+    
+    deleted = []
+    if request.user.is_authenticated() and request.user.has_perm('ref.change_component_instance'):
+        deleted = ComponentInstance.objects.filter(environments__id=envt_id, deleted=True).\
+                    select_related('description').\
+                    order_by('description__name', 'id')
+    
+    sec = (False,)
+    if not envt.protected or (request.user.is_authenticated() and request.user.has_perm('ref.allfields_componentinstance')):
+        sec = (True, False)
+        
     cis = ComponentInstance.objects.filter(environments__id=envt_id, deleted=False).\
                     select_related('description').\
-                    prefetch_related('field_set__field').\
-                    prefetch_related('rel_target_set').\
-                    prefetch_related('description__computed_field_set').\
-                    prefetch_related('description__field_set').\
-                    select_related('instanciates__implements__application')   
-    
+                    select_related('instanciates__implements__application').\
+                    prefetch_related(Prefetch('field_set', queryset=ComponentInstanceField.objects.filter(field__widget_row__gte=0, field__sensitive__in=sec).order_by('field__widget_row'))).\
+                    prefetch_related(Prefetch('description__field_set', queryset=ImplementationFieldDescription.objects.filter(widget_row__gte=0, sensitive__in=sec).order_by('widget_row'))).\
+                    prefetch_related(Prefetch('description__computed_field_set', queryset=ImplementationComputedFieldDescription.objects.filter(widget_row__gte=0, sensitive__in=sec).order_by('widget_row'))).\
+                    order_by('description__tag', 'description__name')
+
     return render(request, 'ref/envt.html', {'envt': envt, 'deleted': deleted, 'cis' : cis})
 
 def backuped(request):
