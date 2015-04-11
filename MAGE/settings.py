a61a65d0ae3b90b9a12e9a2272640df4845931ab
marcanpilami <marsu_pilami@msn.com>
2015-04-11 16:13:32 +0200

    Login link now redirects to current page instead of home

--- MAGE/settings.py
+++ MAGE/settings.py
@@ -111,6 +111,17 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'MAGE/templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.template.context_processors.debug',
+    'django.template.context_processors.i18n',
+    'django.template.context_processors.media',
+    'django.template.context_processors.static',
+    'django.template.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    "django.core.context_processors.request",
+)
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
