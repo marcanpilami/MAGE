c998b041b4ed3649ca8f6003a631f26cbf77c69d
abe <augustin.bessette@gmail.com>
2021-08-30 19:55:34 +0200

    Remove environment variable for path disk ref
    Fix https://github.com/marcanpilami/MAGE/pull/31#discussion_r698641871

--- MAGE/settings.py
+++ MAGE/settings.py
@@ -60,22 +60,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = env_var.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'tmp/media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'tmp/media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = env_var.getenv('MEDIA_URL', '/magefiles/')
+MEDIA_URL = '/magefiles/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = env_var.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'tmp/static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'tmp/static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = env_var.getenv('STATIC_URL', '/static/')
+STATIC_URL = '/static/'
 
 # List of finder classes that know how to find static files in
 # various locations.
