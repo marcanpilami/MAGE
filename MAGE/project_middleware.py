55bb24a3d049c4c6067b6af52a0d6d530f44e1f8
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2022-10-01 18:43:17 +0200

    Projects are now cached

--- MAGE/project_middleware.py
+++ MAGE/project_middleware.py
@@ -1,5 +1,6 @@
-from ref.models import Project
+from django.core.cache import cache
 
+from ref.models import Project
 
 class ProjectFromProjectIdMiddleware:
     """A very simple middleware which adds a 'project' argument to the view call and template context if the argument 'project_id' was given. Factorizes stupid code, including global cache."""
@@ -11,10 +12,15 @@ class ProjectFromProjectIdMiddleware:
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         if not 'project' in view_kwargs and 'project_id' in view_kwargs and view_kwargs['project_id']:
-            try:
-                request.project = Project.objects.get(pk = view_kwargs['project_id'])
-            except (Project.DoesNotExist,ValueError):
-                request.project = Project.objects.get(name = view_kwargs['project_id'])
+            id = view_kwargs['project_id']
+            request.project = cache.get(f'project_{id}')
+
+            if not request.project:
+                try:
+                    request.project = Project.objects.get(pk = id)
+                except (Project.DoesNotExist,ValueError):
+                    request.project = Project.objects.get(name = id)
+                cache.set(f'project_{id}', request.project, 3600)
 
         if 'project_id' in view_kwargs:
             del view_kwargs['project_id']
